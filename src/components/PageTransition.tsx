c213a14494b66c3dd7b3a76e0832854c69ddfc42
CrissyjuanxD <113305236+CrissyjuanxD@users.noreply.github.com>
2025-06-05 23:43:01 -0500

    mediafix

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -5,7 +5,7 @@ const PageTransition: React.FC = () => {
     <div className="page-transition-overlay fixed inset-0 bg-black/70 flex items-center justify-center z-50">
       <div className="flex flex-col items-center">
         <img 
-          src="https://cdn.discordapp.com/attachments/1180691474262798495/1379592668115374110/1115486236723925012_1.gif?ex=6840cd4b&is=683f7bcb&hm=24419731e8d20215b2658c1d138a516bd0e13ed33cc2312bc2495c47763fad84&" 
+          src="https://res.cloudinary.com/dlcusrqqy/image/upload/v1749183649/manubaile_manuweb_i2xgf5.gif" 
           alt="Loading" 
           className="h-24 w-24 spin-element"
         />
