a9d2ebb67625031e49a182b0db8bee00b4725363
Juan Fernando Aviles Guillen <152469383+CrissyjuanUni@users.noreply.github.com>
2026-06-03 15:39:56 -0500

    cambios database

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jjahahpuwyckctzsnoro.supabase.co'; // reemplaza con tu URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqYWhhaHB1d3lja2N0enNub3JvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5ODM2NDEsImV4cCI6MjA2NDU1OTY0MX0.rkfdScc-i155MqkYNkbpIne6ZyncD3YXpXfqbJWFsc4'; // reemplaza con tu anon key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // reemplazar con anon key
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
